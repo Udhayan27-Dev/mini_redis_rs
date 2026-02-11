7b24e1ff32c14cce68a3fce0d48004523d64de33
Udhayan K <kudhayan27@gmail.com>
2026-02-12 00:35:40 +0530

    feat: Converting bytes to string

--- src/resp.rs
+++ src/resp.rs
@@ -37,88 +37,21 @@ fn binary_extract_line(buffer: &[u8], index: &mut usize) -> RESPResult<Vec<u8>> 
     Ok(output)
 }
 
+fn binary_extract_line_as_string(buffer: &[u8], index: &mut usize) -> RESPResult<String> {
+    let line = binary_extract_line(buffer, index)?;
+    Ok(String::from_utf8(line)?)
+}
 
-
-//Test for the RESP Parser
 #[cfg(test)]
 mod tests {
     use super::*;
-    #[test]
-    fn test_binary_extract_line_empty_buffer() {
-        let buffer = "".as_bytes();
-        let mut index: usize = 0;
-        match binary_extract_line(buffer, &mut index) {
-            Err(RESPError::OutOfBounds(index)) => {
-                assert_eq!(index, 0);
-            }
-            _ => panic!(),
-        }
-    }
 
     #[test]
-    fn test_binary_extract_line_single_character() {
-        let buffer = "O".as_bytes();
-        let mut index: usize = 0;
-        match binary_extract_line(buffer, &mut index) {
-            Err(RESPError::OutOfBounds(index)) => {
-                assert_eq!(index, 1);
-            }
-            _ => panic!(),
-        }
-    }
-
-    #[test]
-    fn test_binary_extract_line_index_too_advanced() {
-        let buffer = "OK".as_bytes();
-        let mut index: usize = 1;
-        match binary_extract_line(buffer, &mut index) {
-            Err(RESPError::OutOfBounds(index)) => {
-                assert_eq!(index, 2);
-            }
-            _ => panic!(),
-        }
-    }
-
-    #[test]
-    fn test_binary_extract_line_no_separator() {
-        let buffer = "OK".as_bytes();
-        let mut index: usize = 0;
-        match binary_extract_line(buffer, &mut index) {
-            Err(RESPError::OutOfBounds(index)) => {
-                assert_eq!(index, 2);
-            }
-            _ => panic!(),
-        }
-    }
-
-    #[test]
-    fn test_binary_extract_line_half_separator() {
-        let buffer = "OK\r".as_bytes();
-        let mut index: usize = 0;
-        match binary_extract_line(buffer, &mut index) {
-            Err(RESPError::OutOfBounds(index)) => {
-                assert_eq!(index, 3);
-            }
-            _ => panic!(),
-        }
-    }
-    #[test]
-    fn test_binary_extract_line_incorrect_separator() {
-        let buffer = "OK\n".as_bytes();
-        let mut index: usize = 0;
-        match binary_extract_line(buffer, &mut index) {
-            Err(RESPError::OutOfBounds(index)) => {
-                assert_eq!(index, 3);
-            }
-            _ => panic!(),
-        }
-    }
-    #[test]
-    fn test_binary_extract_line() {
+    fn test_binary_extract_line_as_string() {
         let buffer = "OK\r\n".as_bytes();
         let mut index: usize = 0;
-        let output = binary_extract_line(buffer, &mut index).unwrap();
-        assert_eq!(output, "OK".as_bytes());
+        let output = binary_extract_line_as_string(buffer, &mut index).unwrap();
+        assert_eq!(output, String::from("OK"));
         assert_eq!(index, 4);
     }
 }
